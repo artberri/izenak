4ea4ea60aa2d8e6161c74317921ddef325f919d2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-12 17:54:34 +0000

    Deploy izenak.eus to github.com/artberri/izenak.git:gh-pages

---
+++ precache-manifest.c4479969e21e74e7c2ca685cacdd436a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "86b31eee5d450e4d1d8a",
+    "url": "/js/chunk-vendors.e637b86d.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "27c0d11e356b1082bd46",
+    "url": "/js/izenak.3d193315.js"
+  },
+  {
+    "revision": "3489770d822844c83af6",
+    "url": "/js/app.d825a0ff.js"
+  },
+  {
+    "revision": "b4c0c55e414f0a8e557f",
+    "url": "/js/about.04d4184e.js"
+  },
+  {
+    "revision": "1b6c9e51a0f7d08f89a1a77e4cdca800",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f1b0db08add67d423ab54699a72461d",
+    "url": "/img/icomoon.2f1b0db0.svg"
+  },
+  {
+    "revision": "27c0d11e356b1082bd46",
+    "url": "/css/izenak.9c1bba28.css"
+  },
+  {
+    "revision": "86b31eee5d450e4d1d8a",
+    "url": "/css/chunk-vendors.22898054.css"
+  },
+  {
+    "revision": "3489770d822844c83af6",
+    "url": "/css/app.af717e0a.css"
+  },
+  {
+    "revision": "b4c0c55e414f0a8e557f",
+    "url": "/css/about.af2e63e3.css"
+  }
+];
