c4fbfbfa2f5999ba423e0a521fde1304bdeb9019
Alberto Varela <alberto@berriart.com>
2022-09-23 16:03:05 +0200

    refactor icons

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,9 +1,14 @@
 import { WithChildren } from "../../types/WithChildren"
 import "./Icon.css"
 
-export function Icon({ children }: WithChildren) {
+export interface IconProps extends WithChildren {
+	title?: string
+}
+
+export function Icon({ children, title }: IconProps) {
 	return (
 		<svg class="icon" viewBox="0 0 32 32">
+			{title && <title>{title}</title>}
 			{children}
 		</svg>
 	)
--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -1,5 +1,5 @@
 import { WithChildren } from "../../types/WithChildren"
-import { Icon } from "../Icon/Icon"
+import { BackIcon } from "../BackIcon/BackIcon"
 import "./PageTitle.css"
 
 export function PageTitle({ children }: WithChildren) {
@@ -14,10 +14,7 @@ export function PageTitle({ children }: WithChildren) {
 		<div class="pagetitle">
 			<div class="pagetitle__link">
 				<a href="/" onClick={onClick}>
-					<Icon>
-						<path d="M12.586 27.414l-10-10c-0.781-0.781-0.781-2.047 0-2.828l10-10c0.781-0.781 2.047-0.781 2.828 0s0.781 2.047 0 2.828l-6.586 6.586h19.172c1.105 0 2 0.895 2 2s-0.895 2-2 2h-19.172l6.586 6.586c0.39 0.39 0.586 0.902 0.586 1.414s-0.195 1.024-0.586 1.414c-0.781 0.781-2.047 0.781-2.828 0z"></path>
-					</Icon>{" "}
-					Atzera
+					<BackIcon /> Atzera
 				</a>
 			</div>
 			<h2 class="pagetitle__title">{children}</h2>
